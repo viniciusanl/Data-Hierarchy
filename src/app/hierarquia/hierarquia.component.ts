6aef33c79fa19ee0c62e9088d8d0fca12022768e
Vinícius Andrade Lopes <vinicius.anl@outlook.com>
2019-12-04 21:55:17 -0300

    Implementações de teste

--- src/app/hierarquia/hierarquia.component.ts
+++ src/app/hierarquia/hierarquia.component.ts
@@ -11,16 +11,21 @@ export class HierarquiaComponent implements OnInit {
 
   public folder: any;
   public contaid: any;
+  public contaidfilho: any;
   public position: any;
   public habilitado: any;
+  public hcheck: any;
+  public valorCheckado: any;
   public allfolder = [];
 
   constructor() { }
 
   ngOnInit() {
     this.contaid = 0;
+    this.contaidfilho = 0;
     this.habilitado = true;
     this.position = 0;
+    this.hcheck = false;
   }
 
   async addElement() {
@@ -40,6 +45,7 @@ export class HierarquiaComponent implements OnInit {
         name: 'Pasta',
         description: 'Pasta Pai',
         icon: 'fas fa-folder',
+        habilitado: true,
         filho: [],
       }
     );
@@ -62,9 +68,29 @@ export class HierarquiaComponent implements OnInit {
 
   async addElementF() {
     this.habilitado = false;
+    this.allfolder.forEach(element => {
+      if (element.id === this.valorCheckado) {
+        this.allfolder[this.valorCheckado - 1].filho.push(
+          {
+            id: this.contaidfilho + 1,
+            name: 'Sub Pasta',
+            description: 'Pasta Pai',
+            icon: 'fas fa-folder',
+            habilitado: true,
+            filho: [],
+          }
+        )
+      }
+    });
   }
 
   async remElementF() {
     this.habilitado = true;;
+  }
+
+  async Check(e) {
+    this.valorCheckado = e + 1;
+    let check = document.getElementById(this.valorCheckado) as HTMLElement;
+    console.log(check);
   }
 }
